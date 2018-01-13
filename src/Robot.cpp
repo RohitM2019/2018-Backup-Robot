ffa24b978c237b482eef8f5e456c49b65e719f69
RohitM2019 <rohitmittal@sfhs.com>
2018-01-12 16:39:04 -0800

    Initial Commit

---
+++ src/Robot.cpp
@@ -0,0 +1,70 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+#include <Drive/DifferentialDrive.h>
+#include <Joystick.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include <ctre/Phoenix.h>
+#include <Spark.h>
+
+const int joystickNum = 0;
+const int rMotorNum = 2;
+const int lMotorNum = 6;
+
+class Robot: public frc::IterativeRobot {
+public:
+private:
+	frc::SendableChooser<std::string> m_chooser;
+	const std::string kAutoNameDefault = "Default";
+	const std::string kAutoNameCustom = "My Auto";
+	std::string m_autoSelected;
+	TalonSRX rMotor { rMotorNum };
+	TalonSRX lMotor { lMotorNum };
+	frc::DifferentialDrive myRobot { rMotor, lMotor };
+	frc::Joystick stick { joystickNum };
+	void RobotInit() {
+		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
+		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
+		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+	}
+
+	void AutonomousInit() override {
+		m_autoSelected = m_chooser.GetSelected();
+		// m_autoSelected = SmartDashboard::GetString(
+		// 		"Auto Selector", kAutoNameDefault);
+		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void AutonomousPeriodic() {
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+
+		}
+	}
+
+	void TeleopInit() {
+		myRobot.ArcadeDrive(0, 0);
+	}
+
+	void TeleopPeriodic() {
+		myRobot.ArcadeDrive(stick.GetY(), stick.GetX());
+	}
+};
+
+START_ROBOT_CLASS(Robot)
