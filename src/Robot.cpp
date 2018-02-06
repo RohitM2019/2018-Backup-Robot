71a777b931615a6ebf0e248204f60231d80eff06
messiscores00 <messiscores00@gmail.com>
2018-02-06 15:57:55 -0800

    Update
    1)encoders not counting
    2) if the motorsafetyhelper isn't removed, _rmotor's controlmode is 15, if you take that out, then getcontrolmode returns 1 like it should.
    3) the reportErrors were all only taking in rMotor as arguments. error reports for lMotor were added.
    4)on first call of AutonPeriodic, both lMotor and rMotor have controlMode 1 like they should, then on second call rMotor hits a timeoutError("Output not updated often enough")(if we set checktimeout to 0 or 50, it still hits a timeout error) and lMotor returns controlMode as 15, which is odd because there are only 9 controlModes.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -26,7 +26,7 @@ public:
 	double pConstant = 1.0/400.0;
 	double iConstant = 0.00;
 	double dConstant = 0.00;
-	int checkTimeout = 2;
+	int checkTimeout = 0;
 	const double TICKS_PER_INCH = 325.95;
 private:
 	WPI_TalonSRX * _rMotor = new WPI_TalonSRX(rMotorNum);
@@ -70,8 +70,8 @@ private:
 		_rMotor->GetSensorCollection().SetQuadraturePosition(0, checkTimeout);
 
 		//sets the safety (need this for FRC)
-		_rMotor->SetSafetyEnabled(true);
-		_lMotor->SetSafetyEnabled(true);
+	//	_rMotor->SetSafetyEnabled(true);
+	//	_lMotor->SetSafetyEnabled(true);
 
 		//Selects which CAN you are using
 		_rMotor->SelectProfileSlot(0,0);
@@ -87,6 +87,7 @@ private:
 		//sets the encoder value to 0 to begin with
 		_lMotor->GetSensorCollection().SetQuadraturePosition(0, checkTimeout);
 		_rMotor->GetSensorCollection().SetQuadraturePosition(0, checkTimeout);
+
 	}
 
 
@@ -97,15 +98,18 @@ private:
 
 		//reports the error for the selected sensor position( 0 = Primary closed-loop)
 		DriverStation::ReportError(std::to_string(_rMotor->GetSelectedSensorPosition(0)));
+		DriverStation::ReportError(std::to_string(_lMotor->GetSelectedSensorPosition(0)));
 	}
 
 	void AutonomousPeriodic()
 	{
 		//Should be in Init
-		_rMotor -> Set(ctre::phoenix::motorcontrol::ControlMode::Position, TICKS_PER_INCH * 12);
-		_lMotor -> Set(ctre::phoenix::motorcontrol::ControlMode::Position, TICKS_PER_INCH * 12);
+		_rMotor->Set(ctre::phoenix::motorcontrol::ControlMode::Position, TICKS_PER_INCH * 12 );
+		_lMotor->Set(ctre::phoenix::motorcontrol::ControlMode::Position, TICKS_PER_INCH * 12 );
 
-		DriverStation::ReportError(std::to_string(_rMotor->GetSelectedSensorPosition(0)) + " Control Mode: " + std::to_string((int) _rMotor->GetControlMode()));
+
+		DriverStation::ReportError(std::to_string(_rMotor->GetSelectedSensorPosition(0)) + " Right Control Mode:: " + std::to_string((int) _rMotor->GetControlMode()));
+		DriverStation::ReportError(std::to_string(_lMotor->GetSelectedSensorPosition(0)) + " Left  Control Mode:: " + std::to_string((int) _lMotor->GetControlMode()));
 	}
 };
 
